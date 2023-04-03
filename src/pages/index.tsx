7e1b88a19c7d1fc66c2cdb1de05ba5622bb2c742
br3ntor <br3ntor@gmail.com>
2023-04-02 20:09:40 -0700

    Working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,21 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   console.log(user);
 
@@ -28,7 +39,11 @@ const CreatePostWizard = () => {
       <input
         className="grow bg-transparent outline-none"
         placeholder="Type some emojis!"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
@@ -53,7 +68,7 @@ const PostView = (props: PostWithUser) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
@@ -65,7 +80,7 @@ const Feed = () => {
   if (postsLoading) return <LoadingPage />;
   if (!data) return <div>Something went wrong</div>;
 
-  const postContent = [...data, ...data]?.map((fullPost) => (
+  const postContent = data.map((fullPost) => (
     <PostView {...fullPost} key={fullPost.post.id} />
   ));
 
