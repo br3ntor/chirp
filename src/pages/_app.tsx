945a51eb3969e11761622fd815341cae9f07b435
br3ntor <br3ntor@gmail.com>
2023-04-21 19:26:11 -0700

    New files for new routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { type AppType } from "next/app";
-
+import Head from "next/head";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -9,6 +9,13 @@ import { Toaster } from "react-hot-toast";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      {/* When you throw head info(tag?) in this file it will be the default */}
+      {/* If otherfiles have head it will override this, or vis versa idk yet */}
+      <Head>
+        <title>Chirp</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
