38c802cfe9aa2fe7857c2a3432f69448aab8e089
br3ntor <br3ntor@gmail.com>
2023-04-23 01:50:15 -0700

    Render posts on profile page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
 
-import { type RouterOutputs, api } from "~/utils/api";
+import { api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import Link from "next/link";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -74,36 +73,6 @@ const CreatePostWizard = () => {
   );
 };
 
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profileImageUrl}
-        className="h-14 w-14 rounded-full"
-        alt={`@${author.username}'s profile picture`}
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{`· ${dayjs(
-              post.createdAt
-            ).fromNow()}`}</span>
-          </Link>
-        </div>
-        <span className="text-2xl">{post.content}</span>
-      </div>
-    </div>
-  );
-};
-
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
@@ -142,16 +111,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-// <main className="flex h-screen justify-center">
-//   <div className="h-full w-full border-x border-slate-200  md:max-w-2xl">
-//     <div className="border-b border-slate-400 p-4">
-//       {!isSignedIn && (
-//         <div className="flex justify-center">
-//           <SignInButton />
-//         </div>
-//       )}
-//       {isSignedIn && <CreatePostWizard />}
-//     </div>
-//     <Feed />
-//   </div>
-// </main>
